739d1c2855abb1816a189777a6fb34f89f231ef9
Beznosikov Nikita <outfitplace2@yandex.ru>
2025-12-05 20:00:22 +0500

    first commit

---
+++ task_2.py
@@ -0,0 +1,21 @@
+class Movies:
+    def __init__(self):
+        self.movies = []
+    
+    def add_movie(movie):
+        self.movies.append(movie)
+
+class Comedy(Movies):
+    def add_movie(self, movie):
+        self.movies.append(movie)
+        return f'Комедии: {self.movies}'
+
+class Drama(Movies):
+    def add_movie(self, movie):
+        self.movies.append(movie)
+        return f'Драмы: {self.movies}'
+
+drama = Drama()
+print(drama.add_movie('Оружейный барон'))
+comedy = Comedy()
+print(comedy.add_movie('Большой куш'))
